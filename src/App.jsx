3945a0ac3904a5de7cd10baeebe1a2a7a3656829
Tonyos187 <tonydaoud2003@gmail.com>
2025-03-27 04:57:39 -0600

    tailwind colors and breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-darkGray">
+      <h1 className="text-4xl font-bold text-lightGreen">Hello, Tailwind in React!</h1>
+    </div>
   )
 }
 
